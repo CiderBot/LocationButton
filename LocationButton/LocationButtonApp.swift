494138a3e5d42a2361ccbff490f05d58af6fc5ba
Steven Yung <steve@redricebowl.com>
2023-10-27 20:46:57 -0700

    Initial Commit

---
+++ LocationButton/LocationButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationButtonApp.swift
+//  LocationButton
+//
+//  Created by Steven Yung on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
