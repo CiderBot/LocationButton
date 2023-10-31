ba749578e9e58001ff6bb0b4abefa778f9774645
Steven Yung <steve@redricebowl.com>
2023-10-31 14:30:21 -0700

    minor structure changes, eliminated groups

--- LocationButton/ContentView.swift
+++ LocationButton/ContentView.swift
@@ -29,89 +29,79 @@ struct ContentView: View {
     var body: some View {
         NavigationStack(path: $path) {
             VStack {
-                // Group: Top
-                Group {
-                    HStack {
-                        Image(systemName: "globe")
-                            .imageScale(.large)
-                        .foregroundStyle(.tint)
-                        Text("\(visibleRegion?.center.latitude ?? 0.0) \(visibleRegion?.center.longitude ?? 0.0)")
-                        Spacer()
-                        Button {
-                            path.append("SearchView")
-                        } label: {
-                            Image(systemName: "magnifyingglass")
-                        }
+                HStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    Text("\(visibleRegion?.center.latitude ?? 0.0) \(visibleRegion?.center.longitude ?? 0.0)")
+                    Spacer()
+                    Button {
+                        path.append("SearchView")
+                    } label: {
+                        Image(systemName: "magnifyingglass")
                     }
                 }
                 
-                // Group: Map
-                Group {
-                    Map(position: $position) {
-                        if locationManager.currentLocation != nil {
-                            Marker("", systemImage: "circle.circle.fill", coordinate: locationManager.currentLocation!.coordinate)
-                                .tint(.blue)
-                        }
-                        if showAppleLoc {
-                            Marker("", systemImage: "apple.logo", coordinate: .appleHQLoc)
-                                .tint(.blue)
-                        }
-                        ForEach(selectedPlaces) { place in
-                            Marker(place.name, coordinate: CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude))
-                        }
+                Map(position: $position) {
+                    if locationManager.currentLocation != nil {
+                        Marker("", systemImage: "circle.circle.fill", coordinate: locationManager.currentLocation!.coordinate)
+                            .tint(.blue)
                     }
-                    .mapStyle(.standard(elevation: .realistic))
-                    .onMapCameraChange { context in
-                        visibleRegion = context.region
+                    if showAppleLoc {
+                        Marker("", systemImage: "apple.logo", coordinate: .appleHQLoc)
+                            .tint(.blue)
                     }
-                    .onChange(of: selectedPlaces, {
-                        // recenter map based on search results
-                        position = .automatic
-                    })
-                    .onChange(of: locationManager.currentLocation, {
-                        userLocation = Visibility.visible
-                        withAnimation {
-                            position = .region(locationManager.currentRegion)
-                        }
-                    })
-                    .mapControls {
-                        MapUserLocationButton()
-                            .mapControlVisibility(userLocation)
-                        MapCompass()
-                        MapScaleView()
+                    ForEach(selectedPlaces) { place in
+                        Marker(place.name, coordinate: CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude))
                     }
                 }
-               
-                // Group: Bottom
-                Group {
-                    HStack {
-                        Button(action: {
-                            showAppleLoc = true
-                            withAnimation {
-                                position = .region(.appleHQReg)
-                            }
-                        }, label: {
-                            Image(systemName: "apple.logo")
-                            Text("Apple HQ")
-                        })
-                        .font(.callout)
-                        .buttonStyle(.borderless)
-                        Spacer()
-                        LocationButton(.currentLocation) {
-                            locationManager.requestLocation()
-                        }
-                        .font(.callout)
-                        .symbolVariant(.fill)
-                        .foregroundColor(.blue)
-                        .tint(.white)
+                .mapStyle(.standard(elevation: .realistic))
+                .onMapCameraChange { context in
+                    visibleRegion = context.region
+                }
+                .onChange(of: selectedPlaces, {
+                    // recenter map based on search results
+                    position = .automatic
+                })
+                .onChange(of: locationManager.currentLocation, {
+                    userLocation = Visibility.visible
+                    withAnimation {
+                        position = .region(locationManager.currentRegion)
                     }
+                })
+                .mapControls {
+                    MapUserLocationButton()
+                        .mapControlVisibility(userLocation)
+                    MapCompass()
+                    MapScaleView()
+                }
+               
+                HStack {
+                    Button(action: {
+                        showAppleLoc = true
+                        withAnimation {
+                            position = .region(.appleHQReg)
+                        }
+                    }, label: {
+                        Image(systemName: "apple.logo")
+                        Text("Apple HQ")
+                    })
+                    .font(.callout)
+                    .buttonStyle(.borderless)
+                    Spacer()
+                    LocationButton(.currentLocation) {
+                        locationManager.requestLocation()
+                    }
+                    .font(.callout)
+                    .symbolVariant(.fill)
+                    .foregroundColor(.blue)
+                    .tint(.white)
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
             .navigationDestination(for: String.self) { view in
                 if view == "SearchView" {
                     // visibleRegion should never be nil, but just in case pass in a region
-                    
                     SearchView(searchRegion: visibleRegion ?? .appleHQReg, selectedPlaces: $selectedPlaces)
                 }
             }
